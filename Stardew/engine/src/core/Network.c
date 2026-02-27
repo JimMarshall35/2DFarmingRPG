54f9eaf7aee6567eecc15c56939f7028eb109953
Jim Marshall <jim.marshall@codethink.co.uk>
2026-02-27 19:44:39 +0000

    fix MatchmakeClientServerUpdate

--- Stardew/engine/src/core/Network.c
+++ Stardew/engine/src/core/Network.c
@@ -729,7 +729,7 @@ static char* MatchmakeClient()
         netcode_sleep( delta_time );
 
         
-        netcode_client_send_packet(client, &gMatchmakingInfo, sizeof(struct MatchMakingMessage));
+        netcode_client_send_packet(client, (const uint8_t*)&gMatchmakingInfo, sizeof(struct MatchMakingMessage));
 
         while ( 1 )     
         {
@@ -767,7 +767,7 @@ game_found:
 
 static void MatchmakeClientServerUpdate(struct netcode_client_t * client)
 {
-    netcode_client_send_packet(client, (uint8_t*)&gMatchmakingInfo, sizeof(struct MatchMakingMessage));
+    netcode_client_send_packet(client, (const uint8_t*)&gMatchmakingInfo, sizeof(struct MatchMakingMessage));
     while ( 1 )     
     {
         int packet_bytes;

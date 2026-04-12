c498be4eca4cee0c93ac62ccd61a0e4cbde376f5
Jim Marshall <jim.marshall@codethink.co.uk>
2026-04-12 20:02:07 +0100

    correct flag passed + stamping script outputs whether baked or not

--- Stardew/game/make_release_version_header.py
+++ Stardew/game/make_release_version_header.py
@@ -3,6 +3,7 @@
 # Written as a python script so it works on windows 
 import subprocess
 import time
+import argparse
 
 def get_git_sha():
     return subprocess.check_output(["git", "log", "-1", "--format=%H"]).decode().strip()
@@ -10,7 +11,18 @@ def get_git_sha():
 def get_git_branch():
     return subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
 
+
+def do_cmd_args():
+    parser = argparse.ArgumentParser(
+                    prog='Stamper',
+                    description='Stamp a release version file',
+                    epilog='Jim Marshall - 2025')
+    parser.add_argument("--baked", action="store_true")
+    args = parser.parse_args()
+    return args
+
 def main():
+    args = do_cmd_args()
     data = ""
     with open('./include/WfVersion.h', 'r') as file:
         data = file.read()
@@ -20,7 +32,7 @@ def main():
         print(f"time: {formatted_time}")
         branch = get_git_branch()
         print(f"branch: {branch}")
-        data = data.replace("BUILD_SHA \"<local_build>\"", f"BUILD_SHA \"{sha}\"")
+        data = data.replace("BUILD_SHA \"<local_build>\"", f"BUILD_SHA \"{sha}{"-baked" if args.baked else ""}\"")
         data = data.replace("BUILD_TIME \"<local_build>\"", f"BUILD_TIME \"{formatted_time}\"")
         data = data.replace("WF_BRANCH \"main\"", f"WF_BRANCH \"{branch}\"")
 

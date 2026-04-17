47351193933123d8df0480e5a51a5bbd32ab12f5
Jim Marshall <jim.marshall@codethink.co.uk>
2026-04-17 20:00:15 +0100

    install conan debug deps

--- Stardew/GetDependenciesConan.bat
+++ Stardew/GetDependenciesConan.bat
@@ -1,4 +1,5 @@
 if not exist build mkdir build
 conan profile detect --force
 cd build
-conan install .. --deployer=full_deploy --deployer-folder=./
+conan install .. -s build_type=Debug --deployer=full_deploy --deployer-folder=./
+conan install .. -s build_type=Release --deployer=full_deploy --deployer-folder=./

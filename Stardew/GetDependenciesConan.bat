a3f7d35ae9c5eb1a82fd7067618ed1977930bae7
Jim Marshall <jim.marshall@codethink.co.uk>
2026-04-18 01:30:45 +0100

    try fix windows debug build

--- Stardew/GetDependenciesConan.bat
+++ Stardew/GetDependenciesConan.bat
@@ -1,5 +1,4 @@
 if not exist build mkdir build
 conan profile detect --force
 cd build
-conan install .. -s build_type=Debug --deployer=full_deploy --deployer-folder=./
-conan install .. -s build_type=Release --deployer=full_deploy --deployer-folder=./
+conan install .. -s build_type=%1 --deployer=full_deploy --deployer-folder=./
